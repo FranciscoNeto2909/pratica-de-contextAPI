8c18d682e6b73b0bd0b2921a2055793a5b703bd7
Francisco Neto <wantedshark@gmail.com>
2022-04-04 20:13:31 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import "./navBar.css"
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src="" alt="" />
+            <ul>
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="page1">Pagina 1</Link></li>
+                <li><Link to="about">Sobre</Link></li>
+            </ul>
+            <div className="login">
+                <img src="https://th.bing.com/th/id/R.2faa4e28713dc962310e9f0047ff18e3?rik=n1VDrWI%2bk2QduA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_311846.png&ehk=8BNi6npflzhZPGZub6urwGF3PWz3kxhXvxZ1RRw7XHQ%3d&risl=&pid=ImgRaw&r=0" alt="login image" />
+                <button>Login</button>
+            </div>
+        </nav>
+    )
+}
