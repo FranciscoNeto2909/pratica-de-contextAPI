7485b990bdc86203963606fe32efff1a2a8805e9
Francisco Neto <wantedshark@gmail.com>
2022-04-05 16:30:43 -0300

    error fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 export default function About() {
     return(
-        <h1>About</h1>
+        <>
+        <h1>Sobre</h1>
+        <h2>Esta é a página sobre</h2>
+        </>
     )
 }
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,7 +6,9 @@ export default function Home() {
     return(
         <>
         <h1>Home</h1>
-        <h2>User:{state.name}</h2>
+        <h2>Seja bem-vindo {state.name}</h2>
+
+        <h2>Esta é a pagina inicial</h2>
         </>
     )
 }
